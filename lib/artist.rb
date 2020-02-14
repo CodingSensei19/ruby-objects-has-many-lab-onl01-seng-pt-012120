ad027d1a9149a4b815425b004bb46740e64999f7
Aaron Wilson <codingsensei19@gmail.com>
2020-02-14 00:04:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,19 @@
-require 'pry'
-
-class Artist
-
-  attr_accessor :name, :songs
-
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
+class Artist 
+  attr_accessor :name 
+  def initialize (name)
+    @name = name 
   end
-
+  def songs
+    Song.all.select {|song|  song.artist == self }
+  end
   def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    song.artist = self 
   end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  def add_song_by_name(title) 
+    song = Song.new(title)
+    song.artist = self #self is current artist
   end
-
   def self.song_count
-    @@song_count
+    Song.all.collect {|song| song.artist == self}.count
   end
-
 end
