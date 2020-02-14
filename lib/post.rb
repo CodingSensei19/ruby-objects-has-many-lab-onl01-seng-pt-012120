63c5fc205dc24f8801051d0b98ccf9d49b652f95
Aaron Wilson <codingsensei19@gmail.com>
2020-02-14 00:05:36 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,23 @@
-class Post
-
-  attr_accessor :title, :author
-  def initialize(title)
+class Post 
+  attr_accessor :title
+  @@all = []
+  def initialize (title)
     @title = title
+    @@all << self
   end
-
+  def author 
+    @author 
+  end
+  def author=(object)
+    @author = object
+  end
+  def self.all
+    @@all
+  end
   def author_name
-    if @author == nil
-      return nil
-    else
+    if self.author == nil 
+      nil
+    else 
       @author.name
     end
   end
